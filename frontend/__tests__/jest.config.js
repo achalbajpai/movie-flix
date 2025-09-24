0dc0aefb2f510f89fe861da83161cb387fc12ce5
Achal <achal5bajpai@gmail.com>
2025-09-24 18:42:34 +0530

    feat: add development tooling and testing setup

---
+++ frontend/__tests__/jest.config.js
@@ -0,0 +1,19 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
