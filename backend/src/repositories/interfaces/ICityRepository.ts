dd62ff8c797e055e8dca6b63d4f756e9cb552cbd
Achal <achal5bajpai@gmail.com>
2025-10-04 23:24:53 +0530

    refactor(backend): remove bus and city modules

--- backend/src/repositories/interfaces/ICityRepository.ts
+++
@@ -1,9 +0,0 @@
-import { City } from '@/models'
-
-export interface ICityRepository {
-  findAll(): Promise<City[]>
-  findByQuery(query: string, limit?: number): Promise<City[]>
-  findById(id: string): Promise<City | null>
-  findByIds(ids: string[]): Promise<City[]>
-  findPopularCities(limit?: number): Promise<City[]>
-}
